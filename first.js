cb6e210d6d2cf733a560d62f079c336d0caf6508
Bagheerathan <bagheerocky@gmail.com>
2021-10-02 22:04:51 +0530

    first

---
+++ first.js
@@ -0,0 +1,13 @@
+document.write("Hi..");
+alert("be positive");
+
+var a = "baghee";
+document.write(a);
+
+var b = 5;
+var c = 5;
+document.write(c+b);
+document.getElementById("id1").innerHTML = 'hey There';//inner fo print the valuve
+
+var age = prompt('what is your age');//get input from dialog box
+document.getElementById("id2").innerHTML = age;
